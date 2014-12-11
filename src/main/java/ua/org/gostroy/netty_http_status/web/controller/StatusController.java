662c645df2dc5b67dd850d0f69ad180bb8b90362
Sergey Panov <gostroy@gmail.com>
2014-12-11 18:30:03 +0200

    change velocity code, change /status view

--- src/main/java/ua/org/gostroy/netty_http_status/web/controller/StatusController.java
+++ src/main/java/ua/org/gostroy/netty_http_status/web/controller/StatusController.java
@@ -5,14 +5,17 @@ import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.util.CharsetUtil;
+import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import ua.org.gostroy.netty_http_status.core.HttpServerInitializer;
 import ua.org.gostroy.netty_http_status.service.StatusInfoService;
 import ua.org.gostroy.netty_http_status.web.internal.Controller;
 
 import java.io.*;
+import java.util.Properties;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
@@ -26,19 +29,21 @@ public class StatusController extends Controller {
     public FullHttpResponse getFullHttpResponse() {
         StatusInfoService statusInfoService = new StatusInfoService();
 
+        Properties properties = new Properties();
+        properties.setProperty("resource.loader", "class");
+        properties.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        VelocityEngine engine = new VelocityEngine(properties);
+
         VelocityContext context = new VelocityContext();
         context.put("countStatistic", statusInfoService.findCountStatistic());
         context.put("ipStatistics", statusInfoService.findIpStatistic());
         context.put("redirectStatistics", statusInfoService.findRedirectStatistic());
         context.put("requestStatistics", statusInfoService.findRequestStatistic(16));
 
-
-        InputStream inputStream = this.getClass().getResourceAsStream(HttpServerInitializer.WEB_CONTENT_PATH + getUri() + HttpServerInitializer.TEMPLATE_SUFFIX);
-        Reader templateReader = new InputStreamReader(inputStream);
+        Template t = engine.getTemplate(HttpServerInitializer.WEB_CONTENT_PATH + getUri() + HttpServerInitializer.TEMPLATE_SUFFIX);
 
         StringWriter swOut = new StringWriter();
-        Velocity.evaluate(context, swOut, "LOG", templateReader);
-        System.out.println(swOut);
+        t.merge(context, swOut);
 
         ByteBuf CONTENT = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer(swOut.toString(), CharsetUtil.UTF_8));
         FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, CONTENT.duplicate());
--- src/main/java/ua/org/gostroy/netty_http_status/web/internal/DefaultController.java
+++ src/main/java/ua/org/gostroy/netty_http_status/web/internal/DefaultController.java
@@ -4,6 +4,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.util.CharsetUtil;
 import org.apache.commons.io.IOUtils;
 import org.apache.velocity.VelocityContext;
@@ -17,13 +18,15 @@ import java.io.Reader;
 import java.io.StringWriter;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 /**
  * Created by Panov Sergey on 12/11/2014.
  */
 public class DefaultController extends Controller {
-    private static final String CONTENT_NOT_FOUND = "Page not found";
+    private final String CONTENT_NOT_FOUND = "Page not found";
+    private HttpResponseStatus httpResponseStatus = NOT_FOUND;
 
     @Override
     public FullHttpResponse getFullHttpResponse() {
@@ -32,12 +35,13 @@ public class DefaultController extends Controller {
         try{
             InputStream inputStream = this.getClass().getResourceAsStream(HttpServerInitializer.WEB_CONTENT_PATH + getUri());
             page_content = IOUtils.toString(inputStream, "UTF-8");
+            httpResponseStatus = OK;
         }catch (Exception e){
             page_content = CONTENT_NOT_FOUND;
         }
 
         ByteBuf CONTENT = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer(page_content, CharsetUtil.UTF_8));
-        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND, CONTENT.duplicate());
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, httpResponseStatus, CONTENT.duplicate());
         return response;
     }
 }
