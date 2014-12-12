d69d850b871d6c362326d0c91465f8b30ae5aadf
Sergey Panov <gostroy@gmail.com>
2014-12-12 20:09:07 +0200

    a little clean

--- src/main/java/ua/org/gostroy/netty_http_status/server/handler/statistic/StatisticOutRawHandler.java
+++ src/main/java/ua/org/gostroy/netty_http_status/server/handler/statistic/StatisticOutRawHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.ChannelPromise;
 import ua.org.gostroy.netty_http_status.model.entity.Request;
 import ua.org.gostroy.netty_http_status.server.HttpServerInitializer;
 
+import java.net.SocketAddress;
+
 /**
  * Created by Panov Sergey on 12/12/2014.
  */
@@ -14,27 +16,24 @@ public class StatisticOutRawHandler extends ChannelOutboundHandlerAdapter {
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-        Request request = (Request) ctx.channel().attr(HttpServerInitializer.REQUEST_KEY).get();
+        Request request = ctx.channel().attr(HttpServerInitializer.REQUEST_KEY).get();
 
         if (msg instanceof ByteBuf) {
             ByteBuf byteBuf = (ByteBuf) msg;
             int size = byteBuf.readableBytes();
-            request.setSentBytes((long) size);
-
-            request.setSentBytes((long) size);
-            Long duration = System.currentTimeMillis() - request.getTime().getTime();
-            request.setTimestamp(duration);
-
-            Long speed = 1000 * (request.getReceivedBytes() + request.getSentBytes()) / request.getTimestamp();
-            request.setSpeed(speed);
-
-            HttpServerInitializer.requestService.save(request);
+            request.setSentBytes((long) size + request.getSentBytes());
         }
+
+        Long duration = System.currentTimeMillis() - request.getTime().getTime();
+        request.setTimestamp(duration);
+
+        Long speed = 1000 * (request.getReceivedBytes() + request.getSentBytes()) / request.getTimestamp();
+        request.setSpeed(speed);
+
+        HttpServerInitializer.requestService.save(request);
 
         super.write(ctx, msg, promise);
     }
-
-
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
